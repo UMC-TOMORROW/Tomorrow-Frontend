2930ec0960b5e1e010ef557b9f9522d27aaf0717
heeyun817 <vmffpfm79@naver.com>
2025-07-01 16:02:04 +0900

    🎉 Init: 프로젝트 초기 세팅

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+const palette = {
+  primary: {
+    primary: '#729A73',
+    primaryLight: '#B8CDB9',
+  },
+  gray: {
+    dark: '#000000', 
+    default: '#555555', 
+    light: '#FFFFFF', 
+  },
+  point: {
+    red: '#EE0606',
+    green: '#729A73',
+  },
+};
+
+export default palette;
