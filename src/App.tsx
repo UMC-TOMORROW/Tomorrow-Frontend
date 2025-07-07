216664036c68313e341b728396708a1c7a63227d
2csmin <2whtnals@naver.com>
2025-07-07 23:17:18 +0900

    ✨Feat: Header, Navbar 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import './App.css'
-import {createBrowserRouter, RouterProvider } from "react-router-dom";
-import NotFoundPage from './pages/NotFoundPage';
-import HomeLayout from './layouts/HomeLayout';
-import HomePage from './pages/HomePage';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
+import HomeLayout from "./layouts/HomeLayout";
+import HomePage from "./pages/HomePage";
+import MyPage from "./pages/MyPage";
+import ApplyStatus from "./pages/ApplyStatus";
 
 const router = createBrowserRouter([
   {
-  path: "/",
-  element: <HomeLayout/>,
-  errorElement: <NotFoundPage/>,
-  children: [
-    {index: true, element: <HomePage/>},
-  ]
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+  {
+    path: "/MyPage",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [{ index: true, element: <MyPage /> }],
+  },
+  {
+    path: "/MyPage/ApplyStatus",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [{ index: true, element: <ApplyStatus /> }],
   },
 ]);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/ApplyStatus.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const ApplyStatus = () => {
+  return (
+    <div>
+      <Header title="내일" />
+      <section></section>
+    </div>
+  );
+};
+
+export default ApplyStatus;
