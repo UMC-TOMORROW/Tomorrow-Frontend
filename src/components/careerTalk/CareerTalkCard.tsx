7d28c7dd57e2aa2579b3e008cbdbae955de2a4a9
heeyun817 <vmffpfm79@naver.com>
2025-07-11 15:06:13 +0900

    ✨ Feat: careerTalkData 수정 (#13)

--- src/components/careerTalk/CareerTalkCard.tsx
+++ src/components/careerTalk/CareerTalkCard.tsx
@@ -8,23 +8,31 @@ interface CareerTalkCardProps {
 
 function CareerTalkCard({ category, title, content }: CareerTalkCardProps) {
   const shouldFill =
-    category === "[무료 자격증 추천]" || category === "[커리어 준비 루트]";
+    category === "무료 자격증 추천" || category === "커리어 준비 루트";
 
   const maxLength = 30; // 제목 + 내용 길이 제한
   const fullText = `${title} ${content}`;
-  const trimmedText = fullText.length > maxLength ? fullText.slice(0, maxLength) + "..." : fullText;
+  const trimmedText =
+    fullText.length > maxLength
+      ? fullText.slice(0, maxLength) + "..."
+      : fullText;
 
   return (
     <div
       className="rounded-xl p-[10px] text-[15px] leading-[22px] shadow-sm"
       style={{
-        backgroundColor: shouldFill ? palette.primary.primaryLight75 : palette.gray.light,
+        backgroundColor: shouldFill
+          ? palette.primary.primaryLight75
+          : palette.gray.light,
         color: palette.gray.dark,
         border: `1px solid ${palette.primary.primary}`,
       }}
     >
-      <strong>{category}</strong> {trimmedText}
-      {fullText.length > maxLength && <span className="font-semibold"> 더보기</span>}
+      <strong>[{category}]</strong>
+      {trimmedText}
+      {fullText.length > maxLength && (
+        <span className="font-semibold"> 더보기</span>
+      )}
     </div>
   );
 }
