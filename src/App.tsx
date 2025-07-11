69f61773c4ddd4a2726445fa530bdeb4c5afa973
heeyun817 <vmffpfm79@naver.com>
2025-07-11 13:24:42 +0900

    🐛 Fix: 충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage";
 import HomeLayout from "./layouts/HomeLayout";
 import HomePage from "./pages/HomePage";
+import OnboardingScreen from "./pages/Onboarding";
+// import SearchPage from "./pages/SearchPage";
 import MyPage from "./pages/Mypage/MyPage";
+import CareerTalkListPage from "./pages/careerTalk/CareerTalkListPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
     errorElement: <NotFoundPage />,
-    children: [{ index: true, element: <HomePage /> }],
-  },
-  {
-    path: "/MyPage",
-    element: <HomeLayout />,
-    errorElement: <NotFoundPage />,
-    children: [{ index: true, element: <MyPage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "onboarding", element: <OnboardingScreen /> },
+      // { path: "search", element: <SearchPage /> },
+      {path: 'careertalk', element: <CareerTalkListPage/>},
+      { path: "MyPage", element: <MyPage /> }
+    ],
   },
 ]);
 
