1558ca8c5e995c7faaf6c3b8cb2e64806d9bbfb8
bsh-ko <ssuh3872@gmail.com>
2025-08-01 16:23:51 +0900

    ✨ 로그인 API 연결

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+let accessToken: string | null = null;
+export const setAccessToken = (t: string | null) => {
+  accessToken = t;
+};
+
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API_URL,
   withCredentials: true,
