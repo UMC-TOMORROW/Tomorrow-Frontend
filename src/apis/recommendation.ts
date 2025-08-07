1f293a8712802bd53c2b8a74c06d324624bb334b
heeyun817 <vmffpfm79@naver.com>
2025-08-08 01:41:44 +0900

    ✨ Feat: 희망 조건 수정 API 연결(#58)

--- src/apis/recommendation.ts
+++ src/apis/recommendation.ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from "./axios";
 import type { GetRecommendationListResponse } from "../types/recommendation";
+import type { UpdatePreferencesRequest, UpdatePreferencesResponse, WorkPreferenceType } from "../types/workPreference";
 
 export const getRecommendations = async (
   size = 8,
@@ -17,4 +18,19 @@ export const getRecommendations = async (
   }>("/api/v1/jobs/recommendations", { params });
 
   return response.data.result;
+};
+
+export const patchPreferences = async (
+  preferences: WorkPreferenceType[]
+): Promise<boolean> => {
+  const requestBody: UpdatePreferencesRequest = {
+    preferences,
+  };
+
+  const response = await axiosInstance.patch<UpdatePreferencesResponse>(
+    "/api/v1/preferences",
+    requestBody
+  );
+
+  return response.data.result.saved;
 };
