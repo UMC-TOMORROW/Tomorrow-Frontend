d904b6223699dbb25af486c0bb250c2f1311c5cc
bsh-ko <ssuh3872@gmail.com>
2025-08-09 00:31:53 +0900

    ✨ 업체 정보 입력 컴포넌트 UI

--- src/components/jobPost/CompanyInfo.tsx
+++ src/components/jobPost/CompanyInfo.tsx
@@ -13,28 +13,36 @@ export default function CompanyInfo() {
   return (
     <div className="w-full flex flex-col gap-4">
       <div>
-        <h2 className="text-sm font-bold mb-2">업체 정보를 알려주세요.</h2>
+        <h2
+          style={{ marginBottom: "20px", fontWeight: "700" }}
+          className="text-[18px] text-[#333] leading-[100%] font-pretendard"
+        >
+          업체 정보를 알려주세요.
+        </h2>
+
+        <p className="font-semibold text-[#333] !mb-2">업체명</p>
         <input
           type="text"
           placeholder="예) 내일"
           value={companyName}
           onChange={(e) => setCompanyName(e.target.value)}
-          className="w-full h-[44px] border border-[#DEDEDE] rounded-[8px] px-3 text-sm text-[#333]"
+          className="w-full h-[44px] border border-[#DEDEDE] rounded-[8px] px-3 text-sm text-[#333] !px-3"
         />
       </div>
 
       <div className="relative">
+        <p className="font-semibold text-[#333] !mb-2">장소</p>
         <input
           type="text"
           placeholder="서울 강서구 00로 000"
           value={address}
           onChange={(e) => setAddress(e.target.value)}
-          className="w-full h-[44px] border border-[#DEDEDE] rounded-[8px] px-3 pr-10 text-sm text-[#333]"
+          className="w-full h-[44px] border border-[#DEDEDE] rounded-[8px] px-3 pr-10 text-sm text-[#333] !px-3"
         />
         <button
           type="button"
           onClick={handleSearch}
-          className="absolute top-1/2 right-3 -translate-y-1/2 text-gray-500"
+          className="absolute top-[70%] right-3 -translate-y-1/2  text-[#707070]"
         >
           <Search size={18} />
         </button>
