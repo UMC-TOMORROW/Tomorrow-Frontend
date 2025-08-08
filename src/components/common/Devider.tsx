a86394b0d0c06048f76f2bd2a2a7e2873e052e24
bsh-ko <ssuh3872@gmail.com>
2025-08-08 21:57:41 +0900

    ✨ 구분선 컴포넌트 생성

---
+++ src/components/common/Devider.tsx
@@ -0,0 +1,9 @@
+export default function Divider() {
+  return (
+    <div className="w-full max-w-[375px] h-[20px] px-[10px] flex items-center">
+      <div className="w-full h-px bg-[#DEDEDE]" />
+    </div>
+  );
+}
+
+// 구분선 컴포넌트
