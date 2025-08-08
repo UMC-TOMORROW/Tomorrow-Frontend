34b0d8f07c9e7db8bb64abc641c5d0757078ddf0
bsh-ko <ssuh3872@gmail.com>
2025-08-08 22:06:03 +0900

    ✨ 일자리 형태 선택 컴포넌트 UI 생성

---
+++ src/components/jobPost/JobTypeSelector.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const JobTypeSelector = () => {
+  const [selected, setSelected] = useState<"기업" | "개인">("개인");
+
+  return (
+    <div className="gap-4 flex flex-col px-4 py-8">
+      <h2
+        style={{ marginTop: "20px", marginBottom: "10px", fontWeight: "700" }}
+        className="text-[18px] text-[#333] leading-[100%] font-pretendard"
+      >
+        어떤 형태로 일자리를 등록하시나요?
+      </h2>
+      <div className="flex gap-4 justify-between">
+        {/* 기업 버튼 */}
+        <button
+          type="button"
+          onClick={() => setSelected("기업")}
+          className={`w-[185px] h-[80px] rounded-[10px] flex flex-col items-center justify-center gap-2
+    ${selected === "기업" ? "bg-[#729A73]" : "bg-white border border-[#D1D5DB]"}`}
+        >
+          <div className={`text-[16px] font-semibold ${selected === "기업" ? "text-white" : "text-[#555555D9]"}`}>
+            기업
+          </div>
+          <div className={`text-[12px] ${selected === "기업" ? "text-white/80" : "text-[#555555D9]"}`}>
+            사업체나 기관이 일손 모집
+          </div>
+        </button>
+
+        {/* 개인 버튼 */}
+        <button
+          type="button"
+          onClick={() => setSelected("개인")}
+          className={`w-[185px] h-[80px] rounded-[10px] px-4 py-2 flex flex-col items-center justify-center gap-2
+            ${selected === "개인" ? "bg-[#729A73]" : "bg-white border border-[#D1D5DB]"}`}
+        >
+          <div className={`text-[16px] font-semibold ${selected === "개인" ? "text-white" : "text-[#555555D9]"}`}>
+            개인
+          </div>
+          <div className={`text-[12px] ${selected === "개인" ? "text-white/80" : "text-[#555555D9]"}`}>
+            개인이 일손 모집
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default JobTypeSelector;
